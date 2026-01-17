ea6fd2dd0a56f3e44c85f7ce8a0bb2a8d9996dd1
Fillan Jay <modafillan77@gmai.com>
2026-01-17 20:47:10 +0800

    added a scaffold script to create matic folders and a readme

--- scaffold.py
+++ scaffold.py
@@ -1,23 +1,34 @@
-#The "Safety Check": Before creating anything, check if the folder already exists.
-#If it does, print a warning message: "Warning: Directory already exists. Skipping creation." and stop the script using sys.exit().
-#The Nested README: Ensure the README.md is created inside the new project folder, not outside of it. (Hint: Use base_dir / "README.md")
 
 import sys
 from pathlib import Path
 from datetime import date
 
+current_date = date.today()
+
 # 1. Get the name from the user via terminal
 # If the user didn't provide a name, use a default
 project_name = sys.argv[1] if len(sys.argv) > 1 else "New-Project"
 
+print(f"the name you chose is {project_name}")
+
 # 2. Convert string to a Path object
-base_dir = Path(project_name)
+folder_name = Path(project_name)
 
 # 3. Create the main folder
-base_dir.mkdir(exist_ok=True)
+if folder_name.exists():
+    print("Stopped creating folder, folder already exists")
+    sys.exit()
+else:
+    folder_name.mkdir(exist_ok=True)
 
 # 4. Loop to create subdirectories
 subfolders = ["src", "docs", "tests"]
 for folder in subfolders:
-    target = base_dir / folder  # This works now because base_dir is a Path!
+    target = folder_name / folder  # This works now because base_dir is a Path!
     target.mkdir(exist_ok=True) # Creates a folder, not a file
+
+
+with open(folder_name/"README.md","w") as readme:
+    readme.write(f"# {project_name}\n initiated on {current_date}")
+
+
